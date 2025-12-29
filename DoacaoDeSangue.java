fbb93c2f67e91bc0dfe5ed5e72714c92ff419a8e
Érica Lopes <eric4alopes@gmail.com>
2025-12-28 22:34:01 -0300

    Estrutura inicial do projeto e mensagem de boas-vindas

---
+++ DoacaoDeSangue.java
@@ -0,0 +1,11 @@
+public class DoacaoDeSangue {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("=================================");
+        System.out.println("   BEM-VINDO AO PROGRAMA DE");
+        System.out.println("        DOAÇÃO DE SANGUE");
+        System.out.println("=================================");
+        
+    }
+}
